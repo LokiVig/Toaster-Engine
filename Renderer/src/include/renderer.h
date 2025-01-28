0b75a5e0c6c083fccbb0cf05426859777290e96b
Är detta mitt namn? <87585550+OMGOMGplays@users.noreply.github.com>
2025-01-28 13:30:51 +0100

    Triangle rendered
    WHY is this damn tutorial paid

--- Renderer/src/include/renderer.h
+++ Renderer/src/include/renderer.h
@@ -15,6 +15,13 @@
 #pragma comment (lib, "d3dx11.lib")
 #pragma comment (lib, "d3dx10.lib")
 
+// Toaster Engine
+#include "vertex.h"
+
+// Macros
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
 class Renderer
 {
 public:
@@ -30,6 +37,8 @@ public:
 
 private:
 	void InitD3D(); // Sets up and initializes Direct3D
+	void InitPipeline(); // Initializes the pipeline
+	void InitGraphics(); // Initializes our graphics stuff
 	void CleanD3D(); // Closes Direct3D and releases memory
 
 public:
@@ -40,7 +49,14 @@ private:
 	ID3D11Device* m_pDev; // Pointer to our Direct3D device interface
 	ID3D11DeviceContext* m_pDevCon; // Pointer to our Direct3D device context
 
-	ID3D11RenderTargetView* m_pBackBuffer;
+	ID3D11RenderTargetView* m_pBackBuffer; // Pointer to our back buffer
+
+	ID3D11InputLayout* m_pLayout; // Pointer to our input layout
+
+	ID3D11Buffer* m_pVBuffer; // Pointer to our vertex buffer
+
+	ID3D11VertexShader* m_pVS; // Pointer to our vertex shader
+	ID3D11PixelShader* m_pPS; // Pointer to our pixel shader
 };
 
 #endif // _RENDERER_H
--- Renderer/src/renderer.cpp
+++ Renderer/src/renderer.cpp
@@ -48,7 +48,6 @@ void Renderer::Initialize(const char* pszTitle)
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
 	wc.lpszClassName = "ToasterEngineWindow";
 
 	// Register the window class
@@ -66,8 +65,8 @@ void Renderer::Initialize(const char* pszTitle)
 		pszTitle, // The title viewable on the window
 		WS_OVERLAPPEDWINDOW, // Some sorta style
 		100, 100, // Position 100, 100
-		wr.right - wr.left, // 1280
-		wr.bottom - wr.top, // 720
+		SCREEN_WIDTH, // 1280
+		SCREEN_HEIGHT, // 720
 		NULL, // No parent
 		NULL, // No menu
 		hInstance, // Our instance
@@ -92,10 +91,13 @@ void Renderer::InitD3D()
 	// Fill the swap chain description struct
 	scd.BufferCount = 1;								// One back buffer
 	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // Use 32-bit color
+	scd.BufferDesc.Width = SCREEN_WIDTH;				// Set the backbuffer width
+	scd.BufferDesc.Height = SCREEN_HEIGHT;				// Set the backbuffer height
 	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;	// How swap chain is to be used
 	scd.OutputWindow = m_window;						// The window to be used
 	scd.SampleDesc.Count = 4;							// How many multisamples
 	scd.Windowed = TRUE;								// Windowed / full-screen mode
+	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; // Allow full-screen switching
 
 	// Create a device, device context and swap chain using the information in the scd struct
 	D3D11CreateDeviceAndSwapChain
@@ -131,10 +133,66 @@ void Renderer::InitD3D()
 
 	viewport.TopLeftX = 0;
 	viewport.TopLeftY = 0;
-	viewport.Width = 1280;
-	viewport.Height = 720;
+	viewport.Width = SCREEN_WIDTH;
+	viewport.Height = SCREEN_HEIGHT;
 
 	m_pDevCon->RSSetViewports(1, &viewport);
+
+	InitPipeline(); // Initialize our pipeline
+	InitGraphics(); // Initialize our graphics
+}
+
+void Renderer::InitPipeline()
+{
+	// Load and compile the two shaders
+	ID3D10Blob* VS, * PS;
+	D3DX11CompileFromFile(L"resources/shaders/shaders.shader", 0, 0, "VShader", "vs_4_0", 0, 0, 0, &VS, 0, 0);
+	D3DX11CompileFromFile(L"resources/shaders/shaders.shader", 0, 0, "PShader", "ps_4_0", 0, 0, 0, &PS, 0, 0);
+
+	// Encapsulate both shaders into shader objects
+	m_pDev->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &m_pVS);
+	m_pDev->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &m_pPS);
+
+	// Set the shader objects
+	m_pDevCon->VSSetShader(m_pVS, 0, 0);
+	m_pDevCon->PSSetShader(m_pPS, 0, 0);
+
+	// Create the input elements
+	D3D11_INPUT_ELEMENT_DESC ied[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
+
+	// Create the input layout
+	m_pDev->CreateInputLayout(ied, 2, VS->GetBufferPointer(), VS->GetBufferSize(), &m_pLayout);
+	m_pDevCon->IASetInputLayout(m_pLayout);
+}
+
+void Renderer::InitGraphics()
+{
+	VERTEX triangle[] =
+	{
+		{  0.0f,  0.5f, 0.0f, D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f)},
+		{ 0.45f, -0.5f, 0.0f, D3DXCOLOR(0.0f, 1.0f, 0.0f, 1.0f)},
+		{-0.45f, -0.5f, 0.0f, D3DXCOLOR(0.0f, 0.0f, 1.0f, 1.0f)}
+	};
+
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(bd));
+
+	bd.Usage = D3D11_USAGE_DYNAMIC;				// Write access access by CPU and GPU
+	bd.ByteWidth = sizeof(VERTEX) * 3;			// Size is the VERTEX struct * 3
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;	// Use as vertex buffer
+	bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE; // Allow CPU to write in buffer
+
+	m_pDev->CreateBuffer(&bd, NULL, &m_pVBuffer); // Create the buffer
+
+	// Filling the vertex buffer
+	D3D11_MAPPED_SUBRESOURCE ms;
+	m_pDevCon->Map(m_pVBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms); // Map the buffer
+	memcpy(ms.pData, triangle, sizeof(triangle));														 // Copy the data
+	m_pDevCon->Unmap(m_pVBuffer, NULL);																 // Unmap the buffer
 }
 
 void Renderer::Update()
@@ -167,7 +225,16 @@ void Renderer::RenderFrame()
 	// Clear the back buffer to a deep blue
 	m_pDevCon->ClearRenderTargetView(m_pBackBuffer, D3DXCOLOR(0.0f, 0.2f, 0.4f, 1.0f));
 
-	// Do 3D rendering on the back buffer
+	// Select which vertex buffer to display
+	UINT stride = sizeof(VERTEX);
+	UINT offset = 0;
+	m_pDevCon->IASetVertexBuffers(0, 1, &m_pVBuffer, &stride, &offset);
+
+	// Select which primitive type we're using
+	m_pDevCon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// Draw the vertex buffer to the back buffer
+	m_pDevCon->Draw(3, 0);
 
 	// Switch the back buffer and the front buffer
 	m_pSwapChain->Present(0, 0);
@@ -188,7 +255,12 @@ void Renderer::Shutdown()
 
 void Renderer::CleanD3D()
 {
+	// Switch to windowed mode
+	m_pSwapChain->SetFullscreenState(FALSE, NULL);
+
 	// Close and release all existing COM objects
+	m_pVS->Release();
+	m_pPS->Release();
 	m_pSwapChain->Release();
 	m_pBackBuffer->Release();
 	m_pDev->Release();
